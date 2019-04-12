f331558b0cb00df545405e66097b565c3c532bae
Dmitriy Litvinov <dilitvinov@avito.ru>
2019-04-12 19:35:46 +0300

    bfs

---
+++ core/matching/matchers/json_partial_match2.go
@@ -0,0 +1,54 @@
+package matchers
+
+import (
+	"encoding/json"
+)
+
+func JsonPartialMatch2(match interface{}, toMatch string) bool {
+
+	var expected, actual map[string]interface{}
+	matchString, ok := match.(string)
+	if !ok {
+		return false
+	}
+	err0 := json.Unmarshal([]byte(matchString), &expected)
+	err1 := json.Unmarshal([]byte(toMatch), &actual)
+	if err0 != nil || err1 != nil {
+		return false
+	}
+
+	return true
+}
+
+func getAllNodesFromMap(current map[string]interface{}) []map[string]interface{} {
+
+	var allNodes = make([]map[string]interface{}, 0, 0)
+
+	allNodes = append(allNodes, current)
+
+	for key, val := range current {
+		if innerMap, ok := val.(map[string]interface{}); ok {
+			allNodes = append(allNodes, getAllNodesFromMap(innerMap)...)
+		} else if innerArray, ok := val.([]interface{}); ok {
+			allNodes = append(allNodes, getAllNodesFromArray(innerArray)...)
+		} else {
+
+		}
+	}
+	return allNodes
+}
+
+func getAllNodesFromArray(current []interface{}) []map[string]interface{} {
+	var allNodes = make([]map[string]interface{}, 0, 0)
+
+	for _, val := range current {
+		if innerMap, ok := val.(map[string]interface{}); ok {
+			allNodes = append(allNodes, getAllNodesFromMap(innerMap)...)
+		} else if innerArray, ok := val.([]interface{}); ok {
+			allNodes = append(allNodes, getAllNodesFromArray(innerArray)...)
+		} else {
+
+		}
+	}
+	return allNodes
+}
